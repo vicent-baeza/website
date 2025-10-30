193d8c62f4ad0b985ac9bae7e8348b737f8346bf
Vicent Baeza <vicentbaeza7@gmail.com>
2025-10-30 14:50:34 +0100

    base website template

---
+++ site/scripts.js
@@ -0,0 +1,54 @@
+// -----------------
+// UTILITY FUNCTIONS
+// -----------------
+
+function trycatch(trybody, errorhandler, defaultReturn = undefined) {
+    try {
+        return trybody()
+    }
+    catch(error) {
+        errorhandler(error)
+        return defaultReturn
+    }
+}
+
+function localstorage_errors(trybody) {
+    return trycatch(trybody, () => {
+        console.warn("LOCAL STORAGE ERRROR!")
+    }, '')
+}
+
+
+// ---------
+// DARK MODE
+// ---------
+
+const darkModeButton = document.getElementById('darkmode-button')
+
+if (localstorage_errors(() => localStorage.getItem('lightmode')) == 'enabled') {
+    document.body.classList.add('lightmode')
+}
+
+darkModeButton.addEventListener('click', () => {
+    document.body.classList.toggle('lightmode')
+    enabled = document.body.classList.contains('lightmode')
+    localstorage_errors(() => {
+        localStorage.setItem('lightmode', enabled ? 'enabled' : 'disabled')
+    })
+})
+
+// add class 'loaded' after 500ms, so that transitions don't trigger on page load
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        document.body.classList.add('loaded')
+    }, 500);
+})
+
+
+// --------------
+// DYNAMIC FOOTER
+// --------------
+
+currentYear = new Date().getFullYear()
+document.getElementById('footer-text').textContent = `© ${currentYear} Vicent Baeza`
+
